3051e9ff7fb8517bf843a4275af94a500c8037cb
Kevin <knphillips01@gmail.com>
2020-04-15 18:18:23 -0500

    mvp

--- queries.sql
+++ queries.sql
@@ -2,8 +2,30 @@
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
 
+select p.id, p.productname, c.categoryname
+from product as p
+join category as c on p.categoryid = c.id;
+
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
+
+select o.id, s.companyname
+from [order] as o
+join shipper as s
+on o.shipvia=s.id
+where o.orderdate < '2012-08-09';
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
+select productname, quantity 
+from orderdetail as od
+join product as p
+on od.productid=p.id
+where od.orderid='10251'
+order by productname;
+
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+
+select o.id as OrderID, c.companyname, e.LastName as EmployeeLastName
+from [order] as o
+join customer as c on o.customerid = c.id
+join employee as e on o.employeeid= e.id;
